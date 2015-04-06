26dff4b64f1bfb674839e2fc3dceb714cebaca7b
gio <giorgi.bibilashvili89@gmail.com>
2015-04-06 17:24:29 +0400

    --

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+require_once("telesign.php");
 
-require 'telesign.php';
+$customer_id = '6470CD34-7794-4543-A70F-AFBD04DF53C5';
+$secret_key = 'yvPPGgvPbYfBI65juyXVkNoI0lSFa82K4YxzgbTOoYl4UXPn/WFY6KZzPBTjAtGk7TwEfPUgoXGlHv7xVUh31Q==';
 
-$telesign = new TeleSign();
+$verify = new Verify($customer_id, $secret_key);
 
 $digits = 7;
 $code = rand(pow(10, $digits-1), pow(10, $digits)-1);
 
-
-$telesign->sendSMS($code);
+$phone_number = "13103409700";
+$result = $verify->sms($phone_number, $code);
 
 echo '<pre>';
-print_r($telesign);
+print_r($result);
 die;
-
